7f0ceffb034404905248992aafafefbf2debe5b7
abdallahelmesery213 <abdallahelmesery@gmail.com>
2022-10-30 23:29:52 +0200

    icon and button toggle

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,9 +2,13 @@
 
 import { NgModule } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatIconModule} from '@angular/material/icon';
+
+
 
 @NgModule({
   imports: [],
-  exports: [MatButtonModule]
+  exports: [MatButtonModule, MatButtonToggleModule, MatIconModule]
 })
 export class MaterialModule { }
