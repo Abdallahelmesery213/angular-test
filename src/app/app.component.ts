0a89aec965d813f4d77897b2161c0e844ec62611
abdallahelmesery213 <abdallahelmesery@gmail.com>
2022-11-11 01:47:11 +0200

    snackbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(private snackBar: MatSnackBar){}
   title = 'my-app';
   spinner = false;
   loadData(){
@@ -21,4 +23,11 @@ export class AppComponent {
     {name: "react"},
     {name: "vue"}
   ]
+  filterSatSunDay(date:any){
+    const day = date.getDay();
+    return day !=0 && day!=6
+  }
+  openSnackbar(msg: string, action:string){
+    this.snackBar.open(msg, action, {duration:2000});
+  }
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -16,13 +16,17 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatSnackBarModule} from "@angular/material/snack-bar"
 
 
 @NgModule({
   imports: [],
   exports: [MatButtonModule, MatButtonToggleModule, MatIconModule, MatBadgeModule, MatProgressSpinnerModule,
     MatSidenavModule, MatMenuModule, MatListModule, MatGridListModule,MatTabsModule,MatStepperModule,
-    MatFormFieldModule, MatInputModule, MatSelectModule,MatAutocompleteModule
+    MatFormFieldModule, MatInputModule, MatSelectModule,MatAutocompleteModule, MatDatepickerModule,
+    MatNativeDateModule, MatSnackBarModule
   ]
 })
 export class MaterialModule { }
